3adbe93dc10367555d12d48fdfecb935979edcd8
Rokkubro <lachlanb03@gmail.com>
2023-06-10 02:41:41 +1000

    Enable LTO for linux-frozen

--- .ci/linux-frozen/docker.sh
+++ .ci/linux-frozen/docker.sh
@@ -9,7 +9,7 @@ export CXXFLAGS="-B$HOME/bin/gold $CXXFLAGS"
 
 
 mkdir build && cd build
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DENABLE_LTO=ON
 ninja
 
 ctest -VV -C Release
